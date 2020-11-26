0350d59234954c75d646869ca2cecfab6aee4eea
Artem LD31D <artem.12m21@gmail.com>
2020-11-26 20:23:32 +0200

    Added model 'Article'

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article(models.Model):
+	STATUS_CHOICES = (
+			('draft', 'Draft'),
+			('published', 'Published')
+		)
+
+	title = models.CharField(max_length=256)
+	slug = models.SlugField(max_length=256, unique=True)
+	author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='articles')
+	body = models.TextField()
+	published = models.DateTimeField(default=timezone.now)
+	created = models.DateTimeField(auto_now_add=True)
+	updated = models.DateTimeField(auto_now=True)
+	status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+	class Meta:
+		ordering = ('-published',)
+
+	def __str__(self):
+		return self.title
