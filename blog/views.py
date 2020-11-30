c0ed1395fabfbbc4820e38000f5c0a6b5860469b
Artem LD31D <artem.12m21@gmail.com>
2020-11-30 22:16:59 +0200

    Added form for create comments

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, View
 
 from .models import Article
+from .forms import CommentForm
 
 	
 class ArtileListView(ListView):
@@ -24,5 +25,24 @@ class ArticleView(View):
 
 	def get(self, request, slug):
 		context = self.get_objects(slug)
-		
+		comment_form = CommentForm()
+
+		context.update({'comment_form': comment_form})
+		return render(request, 'blog/article_page/index.html', context=context)
+
+	def post(self, request, slug):
+		context = self.get_objects(slug)
+
+		comment_form = CommentForm(data=request.POST)
+
+		if comment_form.is_valid():
+			new_comment = comment_form.save(commit=False)
+
+			new_comment.article = context['article']
+			new_comment.author = request.user
+
+			new_comment.save()
+
+		comment_form = CommentForm()
+		context.update({'comment_form': comment_form})
 		return render(request, 'blog/article_page/index.html', context=context)
