bb2e2dd289974728f31e7ce19cc3efc605be266b
Artem LD31D <artem.12m21@gmail.com>
2020-12-06 13:13:17 +0200

    Added user autoritation

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+	path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import redirect
-from django.contrib.auth import logout
 from django.views.generic import View
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.forms import AuthenticationForm
 
 
 class LogoutView(View):
@@ -9,3 +11,19 @@ class LogoutView(View):
 	def get(self, request):
 		logout(request)
 		return redirect(self.LOGOUT_URL)
+
+
+class CustomLoginView(LoginView):
+    authentication_form = AuthenticationForm
+    form_class = AuthenticationForm
+    template_name = 'accounts/login/index.html'
+
+    LOGIN_REDIRECT_URL = '/blog/'
+
+    def get_redirect_url(self):
+    	return self.LOGIN_REDIRECT_URL
+
+
+    def form_valid(self, form):
+        login(self.request, form.get_user())
+        return super(LoginView, self).form_valid(form)
