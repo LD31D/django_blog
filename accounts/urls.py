3cae045d0e2ef2ff435417e5de61c6cda5d0dbbb
Artem LD31D <artem.12m21@gmail.com>
2020-12-06 18:12:36 +0200

    Added user registration

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from .views import *
 
 urlpatterns = [
 	path('login/', CustomLoginView.as_view(), name='login'),
+	path('register/', RegisterView.as_view(), name='register'),
     path('logout/', LogoutView.as_view(), name='logout')
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import redirect
-from django.views.generic import View
+from django.views.generic import View, FormView
 from django.contrib.auth import logout, login
 from django.contrib.auth.views import LoginView
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 
 
 class LogoutView(View):
@@ -14,16 +14,28 @@ class LogoutView(View):
 
 
 class CustomLoginView(LoginView):
-    authentication_form = AuthenticationForm
-    form_class = AuthenticationForm
-    template_name = 'accounts/login/index.html'
+	authentication_form = AuthenticationForm
+	form_class = AuthenticationForm
+	template_name = 'accounts/login/index.html'
 
-    LOGIN_REDIRECT_URL = '/blog/'
+	LOGIN_REDIRECT_URL = '/blog/'
 
-    def get_redirect_url(self):
-    	return self.LOGIN_REDIRECT_URL
+	def get_redirect_url(self):
+		return self.LOGIN_REDIRECT_URL
+
+	def form_valid(self, form):
+		user = form.get_user()
+		login(self.request, user)
+		return super(CustomLoginView, self).form_valid(form)
 
 
-    def form_valid(self, form):
-        login(self.request, form.get_user())
-        return super(LoginView, self).form_valid(form)
+class RegisterView(FormView):
+	template_name = 'accounts/register/index.html'
+	form_class = UserCreationForm
+
+	success_url = '/blog/'
+
+	def form_valid(self, form):
+		user = form.save()
+		login(self.request, user)
+		return super(RegisterView, self).form_valid(form)
