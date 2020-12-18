5edba8f806ac46870635ab9bb4955d5d01dd0341
Artem LD31D <artem.12m21@gmail.com>
2020-12-18 17:21:55 +0200

    Added signal to create user profile

--- user_profile/models.py
+++ user_profile/models.py
@@ -5,3 +5,11 @@ from django.db.models.signals import post_save
 
 class UserProfile(models.Model):
 	user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+
+def create_user_profile(sender, **kwargs):
+	if kwargs['created']:
+		user_profile = UserProfile.objects.create(user=kwargs['instance'])
+
+
+post_save.connect(create_user_profile, sender=User)
